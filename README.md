897fc1de4bf213ffe51b8302a3071c318326a318
Tommy W. Major <twm@altere.no>
2022-02-02 21:12:49 +0100

    🤙  project with basic component

---
+++ README.md
@@ -0,0 +1,3 @@
+# React project template
+
+React project that implements a basic component.
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+	return (
+		<>
+			<h1>Min tittel</h1>
+		</>
+	);
+};
+
+export default Title;
